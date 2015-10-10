a1150ff0caed3f986b6452bf1dc00f3e3935a88e
ajar <ajar.interactive@gmail.com>
2015-10-10 17:01:56 +0300

    routes commented

---
+++ routes.js
@@ -0,0 +1,12 @@
+// Router.route('/user', function () {
+//   var item = Items.findOne({_id: this.params._id});
+//   this.render('user', {data: item});
+// });
+// Router.route('/rooms', function () {
+//   var item = Items.findOne({_id: this.params._id});
+//   this.render('rooms', {data: item});
+// });
+// Router.route('/room/:_id', function () {
+//   var item = Rooms.findOne({_id: this.params._id});
+//   this.render('room', {data: item});
+// });
